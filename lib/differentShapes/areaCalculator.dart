2aa31489b848766a0d5b5276d89fd53bb0baa4f5
Pankaj Rajani <rajanipankaj11@gmail.com>
2021-11-24 14:39:33 +0530

    added dark theme and added the shapecontainer

--- lib/differentShapes/areaCalculator.dart
+++ lib/differentShapes/areaCalculator.dart
@@ -12,6 +12,7 @@ class CalculatorApp extends StatelessWidget {
     return MaterialApp(
       title: "Area Calculator",
       theme: ThemeData(
+          brightness: Brightness.dark,
           textTheme: TextTheme(
               bodyText2: TextStyle(fontSize: 20, fontStyle: FontStyle.italic))),
       debugShowCheckedModeBanner: false,
@@ -36,7 +37,7 @@ class _areaCalculatorFormState extends State<areaCalculatorForm> {
   late String result;
   double width = 0;
   double height = 0;
-  String? currentShape;
+  late String currentShape;
   final heightContoller = TextEditingController();
   final widthController = TextEditingController();
   List<String> shapes = ['Triangle', 'Rectangle'];
@@ -65,10 +66,12 @@ class _areaCalculatorFormState extends State<areaCalculatorForm> {
             }).toList(),
             onChanged: (shape) {
               setState(() {
-                currentShape = shape;
+                currentShape = shape!;
               });
             },
           ),
+          //shape
+          ShapeContainer(currentShape),
           AreaTextField(widthController, "Width"),
           AreaTextField(heightContoller, "Height"),
           ElevatedButton(
@@ -131,5 +134,74 @@ class AreaTextField extends StatelessWidget {
               border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10.0))),
         ));
+  }
+}
+
+class ShapeContainer extends StatelessWidget {
+  final String shape;
+  const ShapeContainer(this.shape);
+
+  @override
+  Widget build(BuildContext context) {
+    if (shape == 'Triangle') {
+      return CustomPaint(
+        size: Size(50, 50),
+        painter: TrianglePainter(),
+      );
+    } else if (shape == 'Rectangle') {
+      return CustomPaint(
+        size: Size(50, 50),
+        painter: RectanglePainter(),
+      );
+    }
+    return Container();
+  }
+}
+
+class RectanglePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = Colors.lime.shade100;
+    Rect rect =
+        Rect.fromLTRB(0, size.height / 4, size.width, size.height / 4 * 3);
+    /**Canvas will draw the shape on the application 
+    * Paint is used for styling 
+    * Rect is used for setting path for the  rectangle
+    * Example you describe the rect styling it with Paint and drawing it with Canvas
+    */
+    canvas.drawRect(rect, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class TrianglePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = Colors.teal.shade100;
+    var path = Path();
+    path.moveTo(size.width / 2, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(0, size.width);
+    /**Canvas will draw the shape on the application 
+    * Paint is used for styling 
+    * Path is used to  complex forms using the parts of the path
+    * Example you describe the path styling it with Paint and drawing it with Canvas
+    */
+    canvas.drawPath(path, paint);
+  }
+
+  /**
+   * shouldRepaint is used whether the shape is triggered do we need to same painter to paint the shape
+   */
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
